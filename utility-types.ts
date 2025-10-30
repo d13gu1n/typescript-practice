4ae5a779ebc02f7ec5126bd4e81f7145f33714e8
dacevedom <d.acevedom1993@gmail.com>
2025-10-30 11:26:16 -0300

    Added utility type tests to TS Practice App

--- utility-types.ts
+++ utility-types.ts
@@ -40,4 +40,63 @@ type UserSummary = Pick<User, "name" | "email">;
 const userSummary: UserSummary = { name: "ram", email: "ram@example.com" };
 console.log("🚀 ~ userSummary:", userSummary);
 
-// TODO: Continue Utility Types Lesson -> https://www.geeksforgeeks.org/typescript/typescript-utility-types
+// Parameters Type test
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+type SumParams = Parameters<typeof sum>;
+const params: SumParams = [1, 2];
+
+// Testing extracted parameters with an Array.reduce()
+const result: number = params.reduce((pv, nx) => sum(pv, nx));
+console.log("🚀 ~ params:", params);
+console.log("🚀 ~ result:", result);
+
+function tripleSpeed(mcqueen: number, francesco: number, isItTriple: boolean): string {
+    if (mcqueen * 3 === francesco) {
+        if (isItTriple === false) {
+            isItTriple = true;
+        }
+        return "It is triple speed"
+    }
+    if (mcqueen === 0 || francesco === 0) {
+        return "Another one bites the dust!!";
+    }
+    let speedPercent = 100 - ((mcqueen / francesco) * 100);
+    if (isItTriple === true) {
+        isItTriple = false;
+    }
+    if (speedPercent > 0)
+        return `Lightning McQueen is ${Math.abs(speedPercent)}% slower than Francesco Bernoulli.` 
+    return `Lightning McQueen is ${Math.abs(speedPercent)}% faster than Francesco Bernoulli.`
+}
+
+console.log(tripleSpeed(205, 210, false));
+
+// Record utility Type test
+type Fruit = "apple" | "banana" | "orange";
+type Inventory = Record<Fruit, number>;
+
+const inventory: Inventory = {
+    apple: 10,
+    banana: 15,
+    orange: 20
+};
+/* const anuddaInventory: Inventory = {
+    apple: 5,
+    banana: 9,
+    orange: 25,
+    apricot: 2 // Apricot doesn't exist in Inventory
+} */
+console.log("🚀 ~ inventory:", inventory);
+// console.log("🚀 ~ anuddaInventory:", anuddaInventory);
+
+//Exclude utility Type test
+type Status = "pending" | "approved" | "rejected";
+type NonRejectedStatus = Exclude<Status, "rejected">;
+
+const anuddaStatus: NonRejectedStatus = "approved";
+// const malformedStatus: NonRejectedStatus = "rejected"; This will throw an error
+console.log("🚀 ~ anuddaStatus:", anuddaStatus);
+// console.log("🚀 ~ malformedStatus:", malformedStatus);
