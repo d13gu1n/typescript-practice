b2aabd734f91129ea43a94ed70cd66f9f93d47ae
dacevedom <d.acevedom1993@gmail.com>
2025-11-03 11:44:33 -0300

    Added mathUtils imports to TS Practice App

---
+++ mathUtils.ts
@@ -0,0 +1,9 @@
+function anuddaAdd(a: number, b: number): number {
+    return a + b;
+}
+
+function subtract(a: number, b: number): number {
+    return a - b;
+}
+
+export = { anuddaAdd, subtract }
--- modules.ts
+++ modules.ts
@@ -6,3 +6,12 @@ console.log("🚀 ~ result:", result);
 
 /* Namespace importing test cannot be done here - apparently Verbatim Module Syntax is not meant for exporting methods without 
 a class, which exits the scope of the Geeks4Geeks lesson */
+
+// mathUtils Methods Import test
+import mathUtils = require("./mathUtils");
+
+const sum = mathUtils.anuddaAdd(5, 3);
+const difference = mathUtils.subtract(5, 3);
+
+console.log(`Sum: ${sum}`);
+console.log(`Difference: ${difference}`);
