df997ecad1bab7a4d21e2fdd9368432eae06a194
dacevedom <d.acevedom1993@gmail.com>
2025-11-03 11:39:14 -0300

    Attempted to import methods directly from namespace to modules.ts - FAILED

--- modules.ts
+++ modules.ts
@@ -1,4 +1,8 @@
+// Math importing test
 import math = require("./math");
 
 const result = math.add(5, 10);
 console.log("🚀 ~ result:", result);
+
+/* Namespace importing test cannot be done here - apparently Verbatim Module Syntax is not meant for exporting methods without 
+a class, which exits the scope of the Geeks4Geeks lesson */
